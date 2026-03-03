94b18bf04078a632d2494dd7be51f1c9d211f932
Krishna Gupta <krishnaanew20@gmail.com>
2026-03-03 13:04:34 +0530

    hello

---
+++ src/main/java/mytrip/mytrip/Waterways.java
@@ -0,0 +1,13 @@
+package mytrip.mytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Waterways {
+
+    @GetMapping("/Waterways")
+    public String getData() {
+        return "Please book your ticket for your ship at 30% discount";
+    }
+}
