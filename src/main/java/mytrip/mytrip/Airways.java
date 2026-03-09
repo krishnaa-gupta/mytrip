8d7d117ca19537c0702256a107a8b7e36524dcb1
krishnaa-gupta <krishnaanew20@gmail.com>
2026-03-09 14:43:34 +0530

    Update Airways.java

--- src/main/java/mytrip/mytrip/Airways.java
+++ src/main/java/mytrip/mytrip/Airways.java
@@ -9,6 +9,6 @@ public class Airways {
 
     @GetMapping("/Airays")
     public String getData() {
-        return "Please book your flight ticket on 35% discount";
+        return "Please book your flight ticket on 25% discount";
     }
 }
