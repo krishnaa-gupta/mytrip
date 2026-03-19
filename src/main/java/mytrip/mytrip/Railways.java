17b01983434668317069c11eb20877062653d6a3
Krishna Gupta <krishnaanew20@gmail.com>
2026-03-19 12:04:38 +0530

    message

--- src/main/java/mytrip/mytrip/Railways.java
+++ src/main/java/mytrip/mytrip/Railways.java
@@ -8,6 +8,6 @@ public class Railways {
 
     @GetMapping("/Railways")
     public String getData() {
-        return "Please book your ticket on 60% discount";
+        return "Please book your ticket on 50% discount";
     }
 }
